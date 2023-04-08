bb105cc4ec10e73932198e9f5d2f0f6dd1e9a472
pi8901 <pi8901@gmx.de>
2023-04-08 17:32:26 +0200

    Update 1

--- src/CharCollection.java
+++ src/CharCollection.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -41,41 +43,73 @@ public class CharCollection {
 		return distinctSet.size();
 	}
 
-	int top() {
+	char top() {
 		
-		return 0;
+		char top = list.get(0);
+		int max = 0;
+		int max_o = 0;
+		for(int i = 0; i < list.size(); i++)
+		{
+			char x = list.get(i);
+			for(int j = 0; j < list.size(); j++)
+			{
+				if(x == list.get(j))
+				{
+					max ++;
+				}
+			}
+			
+			if(max > max_o)
+			{
+				max_o = max;
+				top = x;
+			}
+			max = 0;
+		}
+		return top;
 	}
 	 
 	@Override
 	public String toString() {
-		return "CharCollection []";
+		return list.toString();
 	}
 	
-	public CharCollection moreThan() {
+	public CharCollection moreThan(int m) {
 		
-		return null;
+		String out = "";
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(count(list.get(i)) <= m)
+			{
+				out = out + list.get(i);
+			}
+		}
+		return new CharCollection(out);
 	}
 	
-	public boolean equals() {
+	public boolean equals(Object x) {
 		
-		return false;
+		return (list.containsAll((Collection<?>) x));
 	}
 	
 	CharCollection except(CharCollection cc) {
-		return null;
+		
+		list.removeAll(cc.list);
+		System.out.println(list);
+		
+		return new CharCollection(list.toString());
 	}
 	
 	boolean isSubset(CharCollection cc) {
-		return false;
+		return list.is
 	}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CharCollection cc = new CharCollection('A', 'B', 'R', 'A', 'K', 'A', 'D', 'A', 'B', 'R', 'A');
-		System.out.println(cc.size());
-		System.out.println(cc.count('A'));
-		System.out.println(cc.different());
+		CharCollection cc = new CharCollection('m','x','B', 'R', 'A','m', 'K', 'A','m', 'D','m', 'A', 'B','m', 'R', 'A','m');
+		CharCollection cd = new CharCollection('B', 'R', 'x');
+		System.out.println(cc.isSubset(cd));
 	}
 
 }
