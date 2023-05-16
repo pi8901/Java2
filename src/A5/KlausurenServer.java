27b788cc3acf2acf6c55205dc322e4442486e974
MarcelArnold00 <marcel.arnold.1@web.de>
2023-05-16 09:13:21 +0200

    Aufgabe 5

--- src/A5/KlausurenServer.java
+++ src/A5/KlausurenServer.java
@@ -1,31 +1,41 @@
 package A5;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
 public class KlausurenServer {
-	public static void main(String[] args) {
-	    ServerSocket serverSo = null;
-	    int port = Integer.valueOf(args[0]).intValue();
-	    
-	    try {
-	    	serverSo = new ServerSocket(port);
-	    }
-	    catch (IOException e) {}
-	    
-	    System.out.println("Server lauscht auf Port: "+port);
-	    
-	    int clientNr = 0;
-	    while(true) {
-	    	try {
-				Socket clientSo = serverSo.accept();
-				clientNr++;
-				System.out.println("Mit CLient"+clientNr+ " verbunden");
-				new EchoServerThread(clientSo, clientNr).start();
-			} catch (IOException e) {
-				// TODO: handle exception
-			}
-	    }
-	}
+    public void run(String[] args) throws IOException {
+        
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+        
+        int portNumber = Integer.parseInt(args[0]);
+        
+        try (
+            ServerSocket serverSocket =
+                new ServerSocket(Integer.parseInt(args[0]));
+            Socket clientSocket = serverSocket.accept();     
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);                   
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+           
+            while (( inputLine = in.readLine()) != null&& !inputLine.equalsIgnoreCase("STOP")) {
+            	if (inputLine.equalsIgnoreCase("PUT")) {
+        			out.println("1 "+ inputLine);
+				} else {
+					out.println(inputLine);
+				}
+            }
+			
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
 }
---
+++ src/A5/Main.java
@@ -0,0 +1,13 @@
+package A5;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		KlausurenServer Klaus = new KlausurenServer();
+		Klaus.run(args);
+
+	}
+
+}
